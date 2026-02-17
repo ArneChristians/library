8e60a30f5c8f9111015fa67adc39e2a158d14701
ArneC <christiansarne04@gmail.com>
2026-02-17 18:56:30 +0100

    Refactored the Book constructor
    Refactored the Book constructor to a class with a constructor

--- library.js
+++ library.js
@@ -2,20 +2,23 @@
 const myLibrary = [];
 
 //Book constructor
-function Book(title, author, pages, read) {
-    if(!new.target) {
-        throw Error("You must use the 'new' operator to call this constructor")
+
+class Book {
+    constructor(title, author, pages, read) {
+        if(!new.target) {
+            throw Error("You must use the 'new' operator to call this constructor")
+        }
+
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.id = crypto.randomUUID();
+
+        this.info = function() {
+            return `title + "by " + author + ", " + pages + "pages, " + read`;
+        };
     }
-
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.id = crypto.randomUUID();
-
-    this.info = function() {
-        return `title + "by " + author + ", " + pages + "pages, " + read`;
-    };
 }
 
 // Function to add Books
